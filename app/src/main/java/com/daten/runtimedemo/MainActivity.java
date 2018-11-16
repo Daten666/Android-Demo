765901c29dda5ba97ce5664092d333bf5435bdbc
Daten666 <hkm891031@hotmail.com>
2018-11-16 17:01:00 +0800

    完成相机预览点击事件

--- app/src/main/java/com/daten/runtimedemo/MainActivity.java
+++ app/src/main/java/com/daten/runtimedemo/MainActivity.java
@@ -1,8 +1,13 @@
 package com.daten.runtimedemo;
 
 import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,10 +22,65 @@ public class MainActivity extends AppCompatActivity {
             Manifest.permission.WRITE_CONTACTS};
 
     private View mLayout;
+
+    public void showCamera(View view) {
+        Log.i(TAG, "Show camera button pressed. Checking permission.");
+        //检查是否已经获取到相机权限
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // 相机权限没有获取
+            requestCameraPermission();
+
+        } else {
+            Log.i(TAG,
+                    "CAMERA permission has already been granted. Displaying camera preview.");
+            showCameraPreview();
+        }
+    }
+
+    private void showCameraPreview() {
+
+    }
+
+    /**
+     * 获取相机权限
+     */
+    private void requestCameraPermission() {
+        Log.i(TAG, "CAMERA permission has NOT been granted. Requesting permission.");
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+
+            Log.i(TAG,
+                    "Displaying camera permission rationale to provide additional context.");
+            Snackbar.make(mLayout, R.string.permission_camera_rationale,
+                    Snackbar.LENGTH_INDEFINITE)
+                    .setAction(R.string.ok, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[]{Manifest.permission.CAMERA},
+                                    REQUEST_CAMERA);
+                        }
+                    })
+                    .show();
+        } else {
+
+            // Camera permission has not been granted yet. Request it directly.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    REQUEST_CAMERA);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mLayout = findViewById(R.id.sample_main_layout);
+        if (savedInstanceState == null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            RuntimePermissionsFragment fragment = new RuntimePermissionsFragment();
+            transaction.add(R.id.sample_content_fragment,fragment);
+            transaction.commit();
+        }
     }
 }
---
+++ app/src/main/java/com/daten/runtimedemo/RuntimePermissionsFragment.java
@@ -0,0 +1,35 @@
+package com.daten.runtimedemo;
+
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RuntimePermissionsFragment extends Fragment {
+
+
+    public RuntimePermissionsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, null);
+
+        if (Build.VERSION.SDK_INT < 23) {
+
+            root.findViewById(R.id.button_contacts).setVisibility(View.GONE);
+        }
+        return root;
+    }
+
+}
