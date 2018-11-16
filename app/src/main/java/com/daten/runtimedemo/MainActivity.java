8b8d4e932c4dd825f3094db243225f6e10c685f3
Daten666 <hkm891031@hotmail.com>
2018-11-16 15:57:10 +0800

    MainActivity布局

--- app/src/main/java/com/daten/runtimedemo/MainActivity.java
+++ app/src/main/java/com/daten/runtimedemo/MainActivity.java
@@ -1,10 +1,22 @@
 package com.daten.runtimedemo;
 
+import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "MainActivity";
+    //请求相机权限ID
+    private static final int REQUEST_CAMERA = 0;
+    //请求联系人权限ID
+    private static final int REQUEST_CONTACTS = 1;
+
+    private static String[] PERMISSIONS_CONTACT = {Manifest.permission.READ_CONTACTS,
+            Manifest.permission.WRITE_CONTACTS};
+
+    private View mLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
